aea6888aa124d5f7283c4865b0a9063d8298629a
hiepcd <cdh1983@gmail.com>
2016-04-07 18:41:29 +0800

    Hiep

--- vargenerator.py
+++ vargenerator.py
@@ -2,7 +2,6 @@ class Generator:
     def __init__(self):
         self.countstack = 0
         self.countdata = 0
-        self.countgas = 0
         self.count = 0
 
     def gen_stack_var(self):
@@ -34,6 +33,28 @@ class Generator:
     def gen_owner_store_var(self, position):
         return "Ia_store_" + str(position)
 
-    def get_gas_var(self):
-        self.countgas += 1
-        return "gas_" + str(self.countgas)
+    def gen_gas_var(self):
+        self.count += 1
+        return "gas_" + str(self.count)
+
+    def gen_gas_price_var(self):
+        return "Ip"
+
+    def gen_address_var(self):
+        return "Ia"
+
+    def gen_caller_var(self):
+        return "Is"
+
+    def gen_origin_var(self):
+        return "Io"
+
+    def gen_balance_var(self):
+        self.count += 1
+        return "balance_" + str(self.count)
+
+
+
+
+
+
