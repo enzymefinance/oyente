20f56977c4cd141ac254732fac9d75429b802ace
Hrishi Olickel <hrishioa@gmail.com>
2016-09-12 01:08:44 +0800

    minor

--- symExec.py
+++ symExec.py
@@ -10,6 +10,8 @@ from math import *
 import time
 from global_params import *
 import sys
+import atexit
+
 
 if len(sys.argv) >= 12:
     IGNORE_EXCEPTIONS = int(sys.argv[2])
@@ -92,6 +94,9 @@ def main():
 
     print "Running, please wait..."
 
+
+    print "\t============ Results ==========="
+
     if PRINT_MODE:
         print "Checking for Callstack attack..."
     run_callstack_attack()
@@ -121,7 +126,7 @@ def main():
     if PRINT_MODE:
         print "Results for Reentrancy Bug: " + str(reentrancy_all_paths)
     reentrancy_bug_found = any([v for sublist in reentrancy_all_paths for v in sublist])
-    print "Reentrancy bug exists: %s" % str(reentrancy_bug_found)
+    print "\t  Reentrancy bug exists: %s" % str(reentrancy_bug_found)
     # if reentrancy_bug_found:
         # print "logging re bug"
         # with open(reentrancy_report_file, 'a') as r_report:
@@ -129,6 +134,10 @@ def main():
             
     # print_cfg()
 
+def closing_message():
+    print "\t====== Analysis Completed ======"
+
+atexit.register(closing_message)
 
 
 def build_cfg_and_analyze():
@@ -161,7 +170,7 @@ def detect_time_dependency():
             is_dependant = True
             break
 
-    print "Time Dependency: %s" % is_dependant
+    print "\t  Time Dependency: \t %s" % is_dependant
 
     if REPORT_MODE:
         file_name = sys.argv[1].split("/")[len(sys.argv[1].split("/"))-1].split(".")[0]
@@ -200,9 +209,9 @@ def detect_money_concurrency():
     # if PRINT_MODE: print "All false positive cases: ", false_positive
     if PRINT_MODE: print "Concurrency in paths: ", concurrency_paths
     if len(concurrency_paths) > 0:
-        print "Concurrency found in paths: %s" + str(concurrency_paths)
+        print "\t  Concurrency found in paths: %s" + str(concurrency_paths)
     else:
-        print "Concurrency Bug: False"
+        print "\t  Concurrency Bug: \t False"
     if REPORT_MODE:
         rfile.write("number of path: " + str(n) + "\n")
         # number of FP detected
@@ -1447,7 +1456,7 @@ def run_callstack_attack():
 
     result = check_callstack_attack(instructions)
 
-    print "CallStack Attack: %s" % result
+    print "\t  CallStack Attack: \t %s" % result
 
 
 def print_state(block_address, stack, mem, global_state):
