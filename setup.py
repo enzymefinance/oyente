0ea7944e229fa1de9ae505284f22ebbc36194edf
luongnt95 <luongnt.58@gmail.com>
2017-07-25 16:19:18 +0700

    Update oyente version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='oyente',
-    version='1.0.0',
+    version='0.2.0',
     author='Loi Luu',
     # author_email='',
     url='https://github.com/melonport/oyente',
